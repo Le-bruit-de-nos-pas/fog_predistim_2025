73332b2226832ccd42af884462f37a4f518fb71c
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2026-02-11 18:28:06 +0000

    Update resample_vta_efield.py

--- vta/resample_vta_efield.py
+++ vta/resample_vta_efield.py
@@ -5,3 +5,13 @@ import nibabel as nib
 from nilearn.image import resample_to_img
 
 Current_Folder = os.getcwd()
+
+Mask2mm_Path = os.path.join(Current_Folder, 'Masks/MNI152_2mm_brain_mask.nii.gz')
+Mask05mm_Path = os.path.join(Current_Folder, 'Masks/MNI152_05mm_brain_mask.nii.gz')
+
+VTAsRaw_Path = os.path.join(Current_Folder, 'ROIs/VTAs_Raw/')
+EfieldsRaw_Path = os.path.join(Current_Folder, 'ROIs/Efields_Raw/')
+VTAs2mm_Path = os.path.join(Current_Folder, 'ROIs/VTAs_2mm/')
+Efields2mm_Path = os.path.join(Current_Folder, 'ROIs/Efields_2mm/')
+VTAs05mm_Path = os.path.join(Current_Folder, 'ROIs/VTAs_05mm/')
+Efields05mm_Path = os.path.join(Current_Folder, 'ROIs/Efields_05mm/')
