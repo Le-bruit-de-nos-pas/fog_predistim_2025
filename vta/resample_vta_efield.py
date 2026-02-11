b2b6378a186973ce028fa3b7221b4642af9ae378
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2026-02-11 18:28:17 +0000

    Add paths for 2mm and 05mm resolution masks and VTAs

--- vta/resample_vta_efield.py
+++ vta/resample_vta_efield.py
@@ -15,3 +15,19 @@ VTAs2mm_Path = os.path.join(Current_Folder, 'ROIs/VTAs_2mm/')
 Efields2mm_Path = os.path.join(Current_Folder, 'ROIs/Efields_2mm/')
 VTAs05mm_Path = os.path.join(Current_Folder, 'ROIs/VTAs_05mm/')
 Efields05mm_Path = os.path.join(Current_Folder, 'ROIs/Efields_05mm/')
+
+
+res = '2mm'
+# res = '05mm'
+Mask_Paths = {
+    '2mm': Mask2mm_Path,
+    '05mm': Mask05mm_Path
+}
+VTAs_Paths = {
+    '2mm': VTAs2mm_Path,
+    '05mm': VTAs05mm_Path
+}
+Efields_Paths = {
+    '2mm': Efields2mm_Path,
+    '05mm': Efields05mm_Path
+}
