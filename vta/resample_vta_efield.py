fbfec1fce98d4ee106974974b638ee4b5b55f234
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2026-02-11 18:28:32 +0000

    Implement VTA image resampling process
    Load VTA images, resample them to match the mask image, and save the results.

--- vta/resample_vta_efield.py
+++ vta/resample_vta_efield.py
@@ -31,3 +31,22 @@ Efields_Paths = {
     '2mm': Efields2mm_Path,
     '05mm': Efields05mm_Path
 }
+
+
+mask_image = nib.load(Mask_Paths[res])
+VTAsRaw_Paths = glob.glob(os.path.join(VTAsRaw_Path, '**', '*'), recursive=True)
+
+for VTA_path in VTAsRaw_Paths:
+    img = nib.load(VTA_path)
+    img_data = img.get_fdata()
+    img_data = img_data.astype(np.float32)
+    img = nib.Nifti1Image(img_data, img.affine)
+    
+    resliced_img = resample_to_img(img, mask_image, interpolation='nearest')
+    
+    new_path = VTAs_Paths[res] + "/Lateral/" + os.path.basename(VTA_path).replace('.', f'_{res}.', 1)
+
+    nib.save(resliced_img, new_path)
+    print(new_path)
+    
+print("Resampling done.")
