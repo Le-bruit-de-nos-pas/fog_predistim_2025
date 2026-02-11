c0b72a3841dcbad75b5efecc27ea2520f4f8c662
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2026-02-11 18:26:29 +0000

    Add imports and set up raw data paths
    Added imports and defined paths for raw data directories.

--- vta/extract_stimulations.py
+++ vta/extract_stimulations.py
@@ -1,1 +1,8 @@
+import os
+import glob
+import gzip
+import shutil
 
+Current_Folder = os.getcwd()
+VTAsRaw_Path = os.path.join(Current_Folder, 'ROIs/VTAs_Raw/')
+EfieldsRaw_Path = os.path.join(Current_Folder, 'ROIs/Efields_Raw/')
