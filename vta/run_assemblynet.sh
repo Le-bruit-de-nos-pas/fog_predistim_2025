99a5a617122d299e396b8a905102d7c73d0d8222
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2026-03-07 08:22:00 +0000

    Add Docker run command for AssemblyNet
    Added Docker command to run AssemblyNet with specified parameters.

--- vta/run_assemblynet.sh
+++ vta/run_assemblynet.sh
@@ -7,3 +7,14 @@
 # chmod +x run_assemblynet.sh
 # ./run_assemblynet.sh
 #
+sudo docker run --rm \
+-v /mnt/c/Users/paulo/MRI/inputs:/data \
+-v /mnt/c/Users/paulo/MRI/outputs:/data_out \
+-e CUDA_VISIBLE_DEVICES="" \
+volbrain/assemblynet:1.0.0 \
+-recursive \
+-pattern-t1 "*.nii*" \
+-global-csv /data_out/global_volumetry_info.csv \
+-no-pdf-report \
+-batch-size 1 \
+/data/ /data_out/
