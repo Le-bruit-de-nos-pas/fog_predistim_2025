42410a673e42d2e626116cbc9f6800b35196f0cc
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2026-03-19 14:50:15 +0000

    Reorganize and clean up mri_volumetric_age.R
    Refactor code to improve readability and structure.

--- volumetrics/mri_volumetric_age.R
+++ volumetrics/mri_volumetric_age.R
@@ -3,3 +3,9 @@ library(tidyverse)
 library(data.table)
 
 # Age
+
+
+DEMOGRAPHIE <- read_xlsx(path="data/Asymmetry_DeepBrainStimulation.xlsx",sheet = "DEMOGRAPHIE ", skip=0, col_types = "text", trim_ws = TRUE)
+DEMOGRAPHIE <- DEMOGRAPHIE %>% select(SUBJID, AGE) 
+DEMOGRAPHIE <- DEMOGRAPHIE[-1,]
+DEMOGRAPHIE$AGE <- as.numeric(DEMOGRAPHIE$AGE)
